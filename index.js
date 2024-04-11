5a2eb7f1ed5d60965dd9ed0c2f988f50589c1ac6
Jordi <jordijs6@gmail.com>
2024-04-11 12:28:07 +0200

    Complete exercise 1

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/*
+
+Try to access to the title element by using the DOM methods. 
+You can try to get it by using its tag name, its class name or its id. 
+Then, try to get its text content only.
+
+*/
+
 function change() {
-    // your code goes here
+    const titleA = document.querySelector("h1")
+    const titleB = document.querySelector(".titleName")
+    const titleC = document.getElementById("titleId")
+    const titleText = titleA.innerText;
+    console.log(titleA)
+    console.log(titleB)
+    console.log(titleC)
+    console.log(titleText)
 }
+
+change()
