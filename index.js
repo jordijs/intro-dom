60b91612224d93f5b8ac666a9f1243f66bcddf85
Jordi <jordijs6@gmail.com>
2024-04-15 10:25:35 +0200

    unfinished

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/*
+We are going to modify the article's content by adding whatever you want instead of the red square.
+
+Challenge: try adding new red squares to the article. How would you do it ? Don't worry about the styles, they are already done for you. You should only worry about how to create them by using the DOM methods.
+
+Have fun and don't be afraid of breaking up everything ! Happy coding !
+*/
 function change() {
     // add red squares to the HTML when the button is clicked
+    const redSquare = document.querySelector(".redSquare").innerHTML
+    let redSquareElement = document.createElement('div')
+    redSquareElement.innerHTML = redSquare
+    document.querySelector("article").appendChild(redSquareElement)
 }
